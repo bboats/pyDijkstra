34d6f54de9b5b704bd01e54e14f064681c88c4f2
Marcos Vinicius <marcosvop12@gmail.com>
2019-07-01 03:02:17 -0300

    documentacao

--- src/pyPrim.py
+++ src/pyPrim.py
@@ -3,8 +3,6 @@
 #Implementacao do algoritmo de prim modificado para levar em conta as vertices possuirem peso proprio
 #Marcos Vinicius de Oliveira Pinto - 00288560
 
-import os
-
 #valor infinito para utilizarmos posteriormente no algoritmo
 infinito = float('inf')
 
@@ -35,56 +33,68 @@ class Grafo:
 						dictArestas[origem+1] = {destino+1 : peso}
 		return dictArestas
 
+
 	def menor_distancia(self):
 		'''determina qual o vertice que possui o menor custo para adicionar no grafo final e adiciona'''
 		dist_total_escolhido = infinito #valor que grava qual a menor distancia ateh o momento
 		vert_escolhido = -1 #numero do vertice escolhido
 
+		#testa todos os vertices ja inclusos na arvore geradora minima
 		for vertice_atual in self.grafoInclui:
+			#checa todos possiveis vertices alcancaveis a partir do vertice ja incluso escolhido
 			for destino,peso in self.arestas[vertice_atual].items():
+				#calcula a distancia total para atingir o vertice nao incluido na arvore
 				dist_total_atual = peso + self.pesosMinimos[vertice_atual-1] + self.vertices[destino]
+				#testa todas para descobrir qual eh o nodo de menor custo para adicionar na arvore
 				if (dist_total_atual  < dist_total_escolhido) and (destino not in self.grafoInclui):
-					dist_total_escolhido = dist_total_atual
-					vert_escolhido = destino
+					dist_total_escolhido = dist_total_atual #atualiza dist_total caso encontre uma opcao de menor custo
+					vert_escolhido = destino # atualiza qual o numero do novo vertice de menor custo 
 
+		#adiciona o custo para atingir o vertice que acaba de ser escolhido a arvore minima
 		self.pesosMinimos[vert_escolhido-1] = dist_total_escolhido
+		#inclui ele na lista de vertices ja inclusos na arvore
 		self.grafoInclui.append(vert_escolhido)
 
-'''
-numVertices = 5 ##iteração ao gerar a matriz
-destinoVertices = [1,2,3,4,5] ##utilizado para output
-##utilizados na geração do grafo
-pesosVertices = [1,1,100,2,2] 
-matrizArestas = [[-1, 2, 1, 3, -1], 
-				[2, -1, 3, -1, -1], 
-				[1, 3, -1, 1, 2], 
-				[3, -1, 1, -1, 4], 
-				[-1, -1, 2, 4, -1]]
-'''
-while 1:
-	numVertices = int(input('quantas vertices?: '))
-	destinoVertices = list(map(int, input('quais os destinos? (separados por espaco)\n').split(' ')))
-	pesosVertices = list(map(int, input('quais os pesos das vertices? (separados por espaco e ordenados)\n').split(' ')))
-	matrizArestas = [] #define futura lista de listas que sera inserida pelo usuario para informar pesos das arestas que saem de cada vertice
-	for i in range(numVertices):
-		matrizArestas.append(list(map(int, input('Peso das arestas que saem do vertice {}? (-1 indica a ausencia da aresta)\n'.format(i+1)).split(' '))))
 
 
 
-
-	teste = Grafo(matrizArestas,pesosVertices)
-
-
-	while -1 in teste.pesosMinimos:
-		teste.menor_distancia()
-		#print(teste.pesosMinimos)
+#############################
+#############################
+###      DRIVER CODE      ###
+#############################
+#############################
 
 
-	for vert in destinoVertices:
-		#print ("Custo de 1 para {}: {}".format(vert,teste.pesosMinimos[vert-1])) #print verbose
-		print(teste.pesosMinimos[vert-1])
+#recebe inputs do usuario
+numVertices = int(input('quantas vertices?: '))
+destinoVertices = list(map(int, input('quais os destinos? (separados por espaco)\n').split(' ')))
+pesosVertices = list(map(int, input('quais os pesos das vertices? (separados por espaco e ordenados)\n').split(' ')))
+matrizArestas = [] #define futura lista de listas que sera inserida pelo usuario para informar pesos das arestas que saem de cada vertice
+
+#recebe iterativamente a matriz de adjacencia dos vertices
+for i in range(numVertices):
+	matrizArestas.append(list(map(int, input('Peso das arestas que saem do vertice {}? (-1 indica a ausencia da aresta)\n'.format(i+1)).split(' '))))
 
 
-	input('encerra')
-	os.system('cls')
+
+#cria um objeto da classe grafo com os dados obtidos
+teste = Grafo(matrizArestas,pesosVertices)
+
+#enquanto a lista de pesos da arvore geradora minima nao for completa (ainda restam vertices nao incluidos)
+while -1 in teste.pesosMinimos:
+	teste.menor_distancia() #chama o metodo menor distancia, que insere o vertice de menor distancia ainda nao incluido na arvore
+
+
+#output
+resultado = ""
+
+#gera iterativamente a string de output consultando a arvore geradora minima	
+for vert in destinoVertices:
+	#print ("Custo de 1 para {}: {}".format(vert,teste.pesosMinimos[vert-1])) #print verbose
+	resultado += str(teste.pesosMinimos[vert-1]) + ' '
+
+resultado = resultado[0:-1] #remove espaco final desnecessario
+
+print(resultado)
+input('encerra')
 
